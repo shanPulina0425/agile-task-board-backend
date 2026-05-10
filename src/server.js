5d4dc983db405ec621f9f504a9273058198f0725
Shan Pulina <shangtav@gmail.com>
2026-05-10 19:43:50 +0530

    feat: implement MongoDB connection setup in the backend

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
