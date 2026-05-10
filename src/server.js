9bf1ebe5183102ae81b68a10aad6d2fbc6e2c478
Shan Pulina <shangtav@gmail.com>
2026-05-10 14:52:00 +0530

    refactor: update server response message and organize dotenv configuration

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
 
 const app = express();
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 
 app.get('/', (req, res) => {
-    res.send("Backend server is running!");
+    res.send('Agile Task Board API is running...');
 });
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
