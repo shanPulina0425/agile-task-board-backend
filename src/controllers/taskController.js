409d343c296d06bf45e7e24053215526c7926668
Shan Pulina <shangtav@gmail.com>
2026-05-11 23:36:06 +0530

    feat: implement task controller with getTasks and createTask functions

---
+++ src/controllers/taskController.js
@@ -0,0 +1,27 @@
+const Task = require('../models/taskModel');
+
+
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+
+module.exports = {
+    getTasks,
+    createTask
+};
