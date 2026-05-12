00930ba10b66a4f882fcbdb51f0dbd6914f8df11
Shan Pulina <shangtav@gmail.com>
2026-05-12 19:58:53 +0530

    feat: implement update and delete task functionalities in task controller and routes

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -21,7 +21,48 @@ const createTask = async (req, res) => {
 };
 
 
+const updateTask = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        
+        
+        const task = await Task.findByIdAndUpdate(id, req.body, { new: true });
+        
+       
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        
+        
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        
+        
+        const task = await Task.findByIdAndDelete(id);
+        
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        
+        
+        res.status(200).json({ message: "Task deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
 module.exports = {
     getTasks,
-    createTask
+    createTask,
+    updateTask,
+    deleteTask
 };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -5,8 +5,11 @@ const taskController = require('../controllers/taskController');
 
 router.get('/', taskController.getTasks);
 
-
 router.post('/', taskController.createTask);
+
+router.put('/:id', taskController.updateTask);
+
+router.delete('/:id', taskController.deleteTask);
 
 
 module.exports = router;
